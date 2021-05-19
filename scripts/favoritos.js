f3ba7090d7dc6b33b1562597ea951ce486308e41
Juan <juancruzardanaz@gmail.com>
2021-05-19 00:46:17 -0300

    ajuste modo nocturno

---
+++ scripts/favoritos.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+    //Modo Nocturno
+    let bdark = document.getElementById("bdark");
+    let body = document.querySelector("body");
+
+    load();
+
+    bdark.addEventListener("click", e => {
+        body.classList.toggle("darkmode");
+        store(body.classList.contains("darkmode"));
+    });
+
+    function load(){
+        let darkmode = localStorage.getItem("darkmode");
+        if (!darkmode){
+            store("false");
+        }else if(darkmode == "true"){
+            body.classList.add("darkmode");
+        }
+    }
+
+    function store(value){
+        localStorage.setItem("darkmode", value)
+    }
+
+    //Barra-de-busqueda
+
+
+}

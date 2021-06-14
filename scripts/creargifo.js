74e14d7ca20ddcb0fa51b0d38005f8595c2e760d
Juan <juancruzardanaz@gmail.com>
2021-06-13 23:19:38 -0300

    ajuste crear gifo

--- scripts/creargifo.js
+++ scripts/creargifo.js
@@ -1,65 +1,94 @@
 window.onload = () => {
-    //Cambio a Modo Nocturno General
-
-    let bdark = document.getElementById("bdark");
-    let modoNocturno = () => {
-        let tema = localStorage.getItem("tema")
-        if(tema == "oscuro"){
-            document.body.classList.toggle("darkmode");
-            bdark.innerHTML = "Modo Diurno";
-            cambioLogos();
-            cambioIconoCrearGifo();
-            cambioLogoFacebook();
-            cambioLogoTwitter();
-            cambioLogoInstagram();
-            cambioCamara();
-            cambioPelicula();
-        }
-        else if (tema == "claro"){
-            document.body.classList.remove("darkmode");
-            bdark.innerHTML = "Modo Nocturno";
-        }else{
-            localStorage.setItem("tema", "claro")
-            bdark.innerHTML = "Modo Nocturno";
-            cambioLogos();
-            cambioIconoCrearGifo();
-            cambioLogoFacebook();
-            cambioLogoTwitter();
-            cambioLogoInstagram();
-            cambioCamara();
-            cambioPelicula();
-        }
-    }
-
-    bdark.addEventListener("click", () => {
-        let tema = localStorage.getItem("tema")
+    //MODO NOCTURNO
+    //MODO NOCTURNO DESKTOP 
+    const bdark = document.getElementById("bdark");
+    bdark.addEventListener('click', () => {
         document.body.classList.toggle("darkmode");
 
-        if(tema == "oscuro"){
-            localStorage.setItem("tema", "claro")
-            bdark.innerHTML = "Modo Nocturno";
-            cambioLogos();
-            cambioIconoCrearGifo();
-            cambioLogoFacebook();
-            cambioLogoTwitter();
-            cambioLogoInstagram();
-            cambioCamara();
-            cambioPelicula();
-        }
-        else if (tema == "claro"){
-            localStorage.setItem("tema", "oscuro")
+        //Guardamos el modo en LocalStorage
+        if(document.body.classList.contains('darkmode')){
+            localStorage.setItem('oscuro', 'true');
             bdark.innerHTML = "Modo Diurno";
             cambioLogos();
             cambioIconoCrearGifo();
             cambioLogoFacebook();
             cambioLogoTwitter();
             cambioLogoInstagram();
-            cambioCamara();
-            cambioPelicula();
+        } else {
+            localStorage.setItem('oscuro', 'false');
+            bdark.innerHTML = "Modo Nocturno";
+            cambioLogos();
+            cambioIconoCrearGifo();
+            cambioLogoFacebook();
+            cambioLogoTwitter();
+            cambioLogoInstagram();
         }
-    })
-    modoNocturno();
+    });
 
+    //Obtenemos el modo actual
+    if(localStorage.getItem('oscuro') === 'true') {
+        document.body.classList.add("darkmode");
+        bdark.innerHTML = "Modo Diurno";
+        cambioLogos();
+        cambioIconoCrearGifo();
+        cambioLogoFacebook();
+        cambioLogoTwitter();
+        cambioLogoInstagram();
+    } else {
+        document.body.classList.remove("darkmode");
+        bdark.innerHTML = "Modo Nocturno";
+        cambioLogos();
+        cambioIconoCrearGifo();
+        cambioLogoFacebook();
+        cambioLogoTwitter();
+        cambioLogoInstagram();
+    };
+
+    //MODO NOCTURNO MOBILE 
+    const mobileDark = document.getElementById("mobileDark");
+    mobileDark.addEventListener('click', () => {
+        document.body.classList.toggle("darkmode");
+
+        //Guardamos el modo en LocalStorage
+        if(document.body.classList.contains('darkmode')){
+            localStorage.setItem('oscuro', 'true');
+            mobileDark.innerHTML = "Modo Diurno";
+            cambioLogosMobile();
+            cambioIconoCrearGifoMobile();
+            cambioLogoFacebookMobile();
+            cambioLogoTwitterMobile();
+            cambioLogoInstagramMobile();
+        } else {
+            localStorage.setItem('oscuro', 'false');
+            mobileDark.innerHTML = "Modo Nocturno";
+            cambioLogosMobile();
+            cambioIconoCrearGifoMobile();
+            cambioLogoFacebookMobile();
+            cambioLogoTwitterMobile();
+            cambioLogoInstagramMobile();
+        }
+    });
+
+    //Obtenemos el modo actual
+    if(localStorage.getItem('oscuro') === 'true') {
+        document.body.classList.add("darkmode");
+        mobileDark.innerHTML = "Modo Diurno";
+        cambioLogosMobile();
+        cambioIconoCrearGifoMobile();
+        cambioLogoFacebookMobile();
+        cambioLogoTwitterMobile();
+        cambioLogoInstagramMobile();
+    } else {
+        document.body.classList.remove("darkmode");
+        mobileDark.innerHTML = "Modo Nocturno";
+        cambioLogosMobile();
+        cambioIconoCrearGifoMobile();
+        cambioLogoFacebookMobile();
+        cambioLogoTwitterMobile();
+        cambioLogoInstagramMobile();
+    };
+
+    //Cambio DESKTOP
     //Cambio de logo Desktop GIFOS
     function cambioLogos() {
         let logoDesktop = document.getElementById("logoDesktop");
@@ -127,25 +156,71 @@ window.onload = () => {
         }
     }
 
-    //Cambio de Camaras
-    function cambioCamara() {
-        let logoCamara = document.getElementById('camara');
-
-        if (bdark.innerHTML == "Modo Nocturno") {
-            logoCamara.setAttribute('src', './img/camara.svg');
+    //Cambio MOBILE
+    //Cambio de logo Desktop GIFOS MOBILE
+    function cambioLogosMobile() {
+        let logoDesktop = document.getElementById("logoDesktop");
+    
+        if (mobileDark.innerHTML == "Modo Nocturno") {
+            logoDesktop.setAttribute('src', './img/logo-desktop.svg');
         } else {
-            logoCamara.setAttribute('src', './img/camara-modo-noc.svg');
+            logoDesktop.setAttribute('src', './img/Logo-modo-noc.svg');
+        }
+    }
+    
+    //Cambio de icono crear Gifs MOBILE
+    function cambioIconoCrearGifoMobile() {
+        let iconoCrearGifo = document.querySelector('.boton-crear-violeta');
+        let iconoCrearGifoHover = document.querySelector('.boton-crear-blanco');
+
+        if (mobileDark.innerHTML == "Modo Nocturno") {
+            iconoCrearGifo.setAttribute('src', './img/button-crear-gifo.svg');
+            iconoCrearGifoHover.setAttribute('src', './img/CTA-crear-gifo-hover.svg');
+        } else {
+            iconoCrearGifo.setAttribute('src', './img/CTA-crear-gifo-modo-noc.svg');
+            iconoCrearGifoHover.setAttribute('src', './img/CTA-crear-gifo-hover-modo-noc.svg');
         }
     }
 
-    //Cambio lata pelicula
-    function cambioPelicula() {
-        let logoPelicula = document.getElementById('pelicula');
+    //Cambio de red social Facebook MOBILE
+    function cambioLogoFacebookMobile() {
+        let logoFacebook = document.getElementById('facebook');
+        let logoFacebookHover = document.getElementById('facebook-hover');
 
-        if (bdark.innerHTML == "Modo Nocturno") {
-            logoPelicula.setAttribute('src', './img/pelicula.svg');
+        if (mobileDark.innerHTML == "Modo Nocturno") {
+            logoFacebook.setAttribute('src', './img/icon_facebook.svg');
+            logoFacebookHover.setAttribute('src', './img/icon_facebook_hover.svg');
         } else {
-            logoPelicula.setAttribute('src', './img/pelicula-modo-noc.svg');
+            logoFacebook.setAttribute('src', './img/icon_facebook.svg');
+            logoFacebookHover.setAttribute('src', './img/icon_facebook_noc.svg');
+        }
+    }
+
+    //Cambio de red social Twitter MOBILE
+    function cambioLogoTwitterMobile() {
+        let logoTwitter = document.getElementById('twitter');
+        let logoTwitterHover = document.getElementById('twitter-hover');
+
+        if (mobileDark.innerHTML == "Modo Nocturno") {
+            logoTwitter.setAttribute('src', './img/icon-twitter.svg');
+            logoTwitterHover.setAttribute('src', './img/icon-twitter-hover.svg');
+        } else {
+            logoTwitter.setAttribute('src', './img/icon-twitter.svg');
+            logoTwitterHover.setAttribute('src', './img/icon_twitter_noc.svg');
+        }
+    }
+
+    //Cambio de red social Instagram MOBILE
+    function cambioLogoInstagramMobile() {
+        let logoInstagram = document.getElementById('instagram');
+        let logoInstagramHover = document.getElementById('instagram-hover');
+
+        if (mobileDark.innerHTML == "Modo Nocturno") {
+            logoInstagram.setAttribute('src', './img/icon_instagram.svg');
+            logoInstagramHover.setAttribute('src', './img/icon_instagram-hover.svg');
+        } else {
+            logoInstagram.setAttribute('src', './img/icon_instagram.svg');
+            logoInstagramHover.setAttribute('src', './img/icon_instagram_noc.svg');
         }
     }
 
